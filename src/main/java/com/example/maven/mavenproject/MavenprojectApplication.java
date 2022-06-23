ef832e259b858b873bc345b9bf2cbb5894a84071
Anjidale <dale.anji@gmail.com>
2022-06-22 20:44:14 -0400

    checking CI 2 try work item

--- src/main/java/com/example/maven/mavenproject/MavenprojectApplication.java
+++ src/main/java/com/example/maven/mavenproject/MavenprojectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MavenprojectApplication {
 
 	//public static void main(String[] args) {
-		SpringApplication.run(MavenprojectApplication.class, args);
+		// SpringApplication.run(MavenprojectApplication.class, args);
 	}
 
 }
